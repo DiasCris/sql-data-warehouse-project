d4a158d0e58aa78e219cd3564de8ba6b844ca023
DiasCris <cristiandeangelodias@gmail.com>
2026-05-05 13:54:40 -0300

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,23 @@
+/*
+=======================================================
+Create Database  and Schemas
+=======================================================
+*/
 
+USE master;
+Go
+
+-- Create Database 'DataWarehouse'
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
